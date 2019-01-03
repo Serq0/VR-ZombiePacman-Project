5653de419553494e93ab6b5e307f38027d19da28
Serq0 <sergiuszjanczura@gmail.com>
2019-01-03 19:03:35 +0100

    Added microphone

---
+++ Assets/Prefabs/Flashlight.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Flashlight : MonoBehaviour
+{
+    public GameObject gameLight;
+    public Text powerText;
+    float startIntensity = 2.0f;
+    float reduceIntensity = 0.1f;
+    float percentagePower = 100;
+    // Start is called before the first frame update
+
+    void ReduceLight()
+    {
+        gameLight.GetComponent<Light>().intensity = gameLight.GetComponent<Light>().intensity - reduceIntensity;
+    }
+
+    void Start()
+    {
+        gameLight.GetComponent<Light>().intensity = startIntensity;
+        InvokeRepeating("ReduceLight", 0.0f, 0.5f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        percentagePower = Mathf.Round(gameLight.GetComponent<Light>().intensity / startIntensity * 100);
+        powerText.text = percentagePower+"%";
+    }
+}
---
+++ Assets/Prefabs/MicInput.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MicInput : MonoBehaviour
+{
+    public Text loudText;
+    public float MicLoudness;
+
+    AudioClip microphoneInput;
+    bool microphoneInitialized;
+    public float sensitivity;
+    public bool flapped;
+
+    public GameObject flashPower;
+
+    private void Flap()
+    {
+        if(flashPower.GetComponent<Light>().intensity<=1.9f)
+        flashPower.GetComponent<Light>().intensity = flashPower.GetComponent<Light>().intensity + 0.1f;
+    }
+
+    private void Start()
+    {
+        //init microphone input
+        if (Microphone.devices.Length > 0)
+        {
+            microphoneInput = Microphone.Start(Microphone.devices[0], true, 999, 44100);
+            microphoneInitialized = true;
+        }
+    }
+
+    private void Update()
+    {
+        int dec = 128;
+        float[] waveData = new float[dec];
+        int micPosition = Microphone.GetPosition(null) - (dec + 1); // null means the first microphone
+        microphoneInput.GetData(waveData, micPosition);
+
+        // Getting a peak on the last 128 samples
+        float levelMax = 0;
+        for (int i = 0; i < dec; i++)
+        {
+            float wavePeak = waveData[i] * waveData[i];
+            if (levelMax < wavePeak)
+            {
+                levelMax = wavePeak;
+            }
+        }
+        float level = Mathf.Sqrt(Mathf.Sqrt(levelMax));
+        if (level > sensitivity) //&& !flapped
+        {
+            Flap();
+            //flapped = true;
+        
+
+    }
+        if (level < sensitivity) //&& flapped
+            //flapped = false;
+
+        loudText.text = level.ToString();
+    }
+
+}
